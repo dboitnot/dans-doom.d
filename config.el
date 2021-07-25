d5ebb95e5a9ead1e4e39287fec709186bdcfc413
Dan Boitnott <dboitnot@gmail.com>
2021-07-25 10:49:06 -0500

    Switched themes to something more readable when working with LISP

--- config.el
+++ config.el
@@ -31,7 +31,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; `doom-one' is the default
-(setq doom-theme 'doom-nord)
+(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
